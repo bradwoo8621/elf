6ff4f22a2e7de27f00baebef21469a88e53f4fd4
botlikes <75356972+botlikes456@users.noreply.github.com>
2026-02-02 16:42:15 +0800

    init

--- pipeline_kernel/src/pipeline/compiled_in_memory_parameter/compiled_constant_parameter.rs
+++ pipeline_kernel/src/pipeline/compiled_in_memory_parameter/compiled_constant_parameter.rs
@@ -1,22 +1,26 @@
-use crate::{ArcTopicDataValue, InMemoryData};
+use crate::{ArcTopicDataValue, DataPath, InMemoryData};
 use elf_base::StdR;
 use elf_model::TenantId;
 use elf_runtime_model_kernel::ArcConstantParameter;
 use std::sync::Arc;
 
-pub struct CompiledConstantParameter;
+pub struct CompiledConstantParameter {
+    path: DataPath,
+}
 
 impl CompiledConstantParameter {
     pub fn compile(
-        _parameter: &Arc<ArcConstantParameter>,
+        parameter: &Arc<ArcConstantParameter>,
         _tenant_id: &Arc<TenantId>,
     ) -> StdR<Self> {
-        Ok(CompiledConstantParameter {})
+        Ok(CompiledConstantParameter {
+            path: DataPath::from_str(parameter.value.as_str())?,
+        })
     }
 }
 
 impl CompiledConstantParameter {
-    pub fn value_from(&self, _in_memory_data: &mut InMemoryData) -> StdR<Arc<ArcTopicDataValue>> {
-        todo!("implement value_from for CompiledConstantParameter")
+    pub fn value_from(&self, in_memory_data: &mut InMemoryData) -> StdR<Arc<ArcTopicDataValue>> {
+        in_memory_data.all_allowed().value_of(&self.path)
     }
 }
